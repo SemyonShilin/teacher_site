f1f480b3a51041d94ab0455391095a41f5d7b462
ShilinSemyon <pipl123gulaet>
2016-10-15 17:10:21 +0300

    added files upload and some fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,31 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.doc do
+        send_data @post.file,
+                  type: 'application/doc',
+                  # filename: "#{@post.title}.doc",
+                  disposition: :inline
+      end
+      format.docx do
+        send_data @post.file,
+                  type: 'application/docx',
+                  filename: "#{@post.title}.docx",
+                  disposition: :inline
+      end
+      format.pdf do
+        send_data @post.file,
+                  type: 'application/pdf',
+                  # filename: "#{@post.title}.pdf",
+                  disposition: :inline
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:file)
   end
 end
