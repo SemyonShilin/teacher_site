c0827cd8760497a19d8f1e979addf60c76379827
SSHilin <sshilin@at-consulting.ru>
2016-11-29 15:39:09 +0300

    f

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
   has_many :images
 
 
-  translates :title, :content
-  globalize_accessors locales: [:ru, :en], attributes: [:title, :content]
+  # translates :title, :content
+  # globalize_accessors locales: [:ru, :en], attributes: [:title, :content]
 
   mount_uploader :file, FileUploader
 end
