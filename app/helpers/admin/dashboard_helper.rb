b9ae3c9df85d4ed232bae9dcc064b0657d21b462
SSHilin <sshilin@at-consulting.ru>
2017-01-26 11:14:17 +0300

    changes

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,13 +1,13 @@
 module Admin::DashboardHelper
   def line_for_posts_count
     posts = {}
-    Post.all.group_by{|e| e.created_at.beginning_of_month}.each{|key, value| posts[key.to_date] = value.count }
+    Post.all.group_by{|e| e.created_at.beginning_of_week}.each{|key, value| posts[key.to_date] = value.count }
     posts
   end
 
   def line_for_users_count
     posts = {}
-    User.all.group_by{|e| e.created_at.beginning_of_month}.each{|key, value| posts[key.to_date] = value.count }
+    User.all.group_by{|e| e.created_at.beginning_of_week}.each{|key, value| posts[key.to_date] = value.count }
     posts
   end
 end
